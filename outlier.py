7e6421ec9e8f96e4d0f66293334279eefc9d5df9
Cathy Qiu <cathyqiujing@gmail.com>
2016-02-14 18:01:14 -0500

    Adding python code for getting stock prices for 5 companies

---
+++ outlier.py
@@ -0,0 +1,8 @@
+# convert our RDD of strings to numeric data so we can compute stats and remove the outliers.
+distanceNumerics = distance.map(lambda string: float(string))
+stats = distanceNumerics.stats()
+stddev = std.stddev()
+mean = stats.mean()
+reasonableDistances = distanceNumerics.filter(
+	lambda x: math.fabs(x-mean) > 2 * stddev)
+print reasonableDistances.collect()
---
+++ yahoo.py
@@ -0,0 +1,23 @@
+import time
+from yahoo_finance import Share
+
+nyt = Share('NYT')
+ibm = Share('IBM')
+google = Share('GOOG')
+facebook = Share('FB')
+linkedin = Share('LNKD')
+
+for minute in range(30):
+	print "%s minutes" % minute
+	nyt.refresh()
+	print "The New York Times' stock price is $%s" % nyt.get_price()
+	ibm.refresh()
+	print "IBM's stock price is $%s" % ibm.get_price()
+	google.refresh()
+	print "Google's stock price is $%s" % google.get_price()
+	facebook.refresh()
+	print "Facebook's stock price is $%s" % facebook.get_price()
+	linkedin.refresh()
+	print "Linkedin's stock price is $%s" % linkedin.get_price()
+	time.sleep(10)
+
