2bce55aac562201c2e2bfea814e00f97f312134d
cathyq <cathyqiujing@gmail.com>
2016-02-16 22:46:03 -0500

    Fix typo

--- outlier.py
+++ outlier.py
@@ -1,7 +1,10 @@
 # convert our RDD of strings to numeric data so we can compute stats and remove the outliers.
+
+
+
 distanceNumerics = distance.map(lambda string: float(string))
 stats = distanceNumerics.stats()
-stddev = std.stddev()
+stddev = stats.stddev()
 mean = stats.mean()
 reasonableDistances = distanceNumerics.filter(
 	lambda x: math.fabs(x-mean) > 2 * stddev)
