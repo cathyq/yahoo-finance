19e44157e9cb31249fe3dc62a6003a5fd9366511
cathyq <cathyqiujing@gmail.com>
2016-02-17 13:37:56 -0500

    Adding print date time for each company

--- yahoo.py
+++ yahoo.py
@@ -11,10 +11,28 @@ for minute in range(30):
 	linkedin = Share('LNKD')
 	
 	print "%s minutes" % minute
+
+	print nyt.get_trade_datetime()
 	print "The New York Times' stock price is $%s" % nyt.get_price()
+
+	print ibm.get_trade_datetime()
 	print "IBM's stock price is $%s" % ibm.get_price()
+
+	print google.get_trade_datetime()
 	print "Google's stock price is $%s" % google.get_price()
+
+	print facebook.get_trade_datetime()
 	print "Facebook's stock price is $%s" % facebook.get_price()
+
+	print linkedin.get_trade_datetime()
 	print "Linkedin's stock price is $%s" % linkedin.get_price()
+
+	
+	nyt.refresh();
+	ibm.refresh();
+	google.refresh();
+	facebook.refresh();
+	linkedin.refresh();
+	
 	time.sleep(60)
 
