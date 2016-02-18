f58fcba8573c42fad6a4600582a38e892ba4bcb4
cathyq <cathyqiujing@gmail.com>
2016-02-17 21:38:12 -0500

    save outlier work

--- outlier.py
+++ outlier.py
@@ -1,7 +1,11 @@
 # convert our RDD of strings to numeric data so we can compute stats and remove the outliers.
 
+from pyspark import SparkContext
+from pyspark.mllib.feature import HashingTF, IDF
+path = '/Users/cathyq/code/yahoo-finance/yahoo_output'
+sc = SparkContext(appName = "yahoo-finance")
 
-
+distance = sc.wholeTextFiles("data")
 distanceNumerics = distance.map(lambda string: float(string))
 stats = distanceNumerics.stats()
 stddev = stats.stddev()
@@ -9,3 +13,4 @@ mean = stats.mean()
 reasonableDistances = distanceNumerics.filter(
 	lambda x: math.fabs(x-mean) > 2 * stddev)
 print reasonableDistances.collect()
+reasonableDistances.saveAsTextFile(path)
