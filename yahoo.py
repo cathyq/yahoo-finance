c2268af91f11bc67c32a9e19bf6ae310c8150be6
cathyq <cathyqiujing@gmail.com>
2016-02-17 21:46:38 -0500

    change yahoo script to take parameter for stock symbol

--- yahoo.py
+++ yahoo.py
@@ -1,38 +1,13 @@
 import time
 from yahoo_finance import Share
+import sys
 
 # Collect 30 mins of Finance data on 5 companies, one value per minute
 
-for minute in range(30):
-	nyt = Share('NYT')
-	ibm = Share('IBM')
-	google = Share('GOOG')
-	facebook = Share('FB')
-	linkedin = Share('LNKD')
-	
-	print "%s minutes" % minute
+share = Share(sys.argv[1])
 
-	print nyt.get_trade_datetime()
-	print "The New York Times' stock price is $%s" % nyt.get_price()
-
-	print ibm.get_trade_datetime()
-	print "IBM's stock price is $%s" % ibm.get_price()
-
-	print google.get_trade_datetime()
-	print "Google's stock price is $%s" % google.get_price()
-
-	print facebook.get_trade_datetime()
-	print "Facebook's stock price is $%s" % facebook.get_price()
-
-	print linkedin.get_trade_datetime()
-	print "Linkedin's stock price is $%s" % linkedin.get_price()
-
-	
-	nyt.refresh();
-	ibm.refresh();
-	google.refresh();
-	facebook.refresh();
-	linkedin.refresh();
-	
+for i in range(30):
+	share.refresh();
+	print share.get_price()
 	time.sleep(60)
 
